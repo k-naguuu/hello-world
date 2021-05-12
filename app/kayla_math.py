ae463444641c13733c21bc79c5ea76d00a9071bb
Kayla Nague <knague@Kaylas-MacBook-Pro.local>
2021-05-11 22:02:53 -0700

    my first math problems

---
+++ app/kayla_math.py
@@ -0,0 +1,19 @@
+""" 
+What's up homie? This module is about math and liam and their gang. 
+I just learned about style tips and best practices. Welcome to my world!
+Style tips are hard. This is called kayla_math.py btw. 
+
+"""
+
+from math import sqrt
+
+def square(x): 
+    return x*x
+
+def hypotenuse (a,b): 
+    
+   c = sqrt(square(a) + square(b))
+   return c
+   
+print (hypotenuse(3,4))
+
